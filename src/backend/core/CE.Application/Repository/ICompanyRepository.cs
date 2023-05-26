521dc1b57e9661db40e4ecb36a27e1a9f8b3889f
Aliyu Abdullahi <aliyuabdullahi074@gmail.com>
2023-05-26 09:37:20 +0100

    Add project files.

---
+++ src/backend/core/CE.Application/Repository/ICompanyRepository.cs
@@ -0,0 +1,18 @@
+﻿using CE.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CE.Application.Repository
+{
+    public interface ICompanyRepository
+    {
+        Task<ICollection<Company>> GetAllCompanies();
+        Task<Company> GetCompanyById(int id);
+        void AddCompany(Company company);
+        void UpdateCompany(Company company);
+        void DeleteCompany(Company company);
+    }
+}
